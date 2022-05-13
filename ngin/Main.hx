c1861aef44acd1cd6715bf10c5d4351a7ef68efa
Luke Hayes <lukepurplehayes@gmail.com>
2022-05-13 08:35:06 +0100

    Reomved comments from TMXParser.hx

--- ngin/Main.hx
+++ ngin/Main.hx
@@ -5,17 +5,12 @@ class Main extends hxd.App {
     var bmp : h2d.Bitmap;
 
     override function init() {
-        // allocate a Texture with red color and creates a 100x100 Tile from it
-        var tile = h2d.Tile.fromColor(0xFF0000, 100, 100);
-        // create a Bitmap object, which will display the tile
-        // and will be added to our 2D scene (s2d)
+        var tile = h2d.Tile.fromColor(0x0000FF, 100, 100);
         bmp = new h2d.Bitmap(tile, s2d);
-        // modify the display position of the Bitmap sprite
         bmp.x = s2d.width * 0.5;
         bmp.y = s2d.height * 0.5;
     }
     override function update(dt:Float) {
-        // increment the display bitmap rotation by 0.1 radians
         bmp.rotation += 0.1;
     }
     static function main() {
