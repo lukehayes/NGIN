a94f919305080e8c15b34b4e3ebe9fcb7a202471
Luke Hayes <lukepurplehayes@gmail.com>
2022-05-13 08:48:53 +0100

    Added TMXParser::getRoot() method and root member.

--- ngin/tilemap/TMXParser.hx
+++ ngin/tilemap/TMXParser.hx
@@ -9,12 +9,26 @@ class TMXParser
 {
     var tmxFile : String;
     var xml     : Xml;
+    var root    : Xml;
 
     public function new(tmxFile:String)
     {
         this.tmxFile = sys.io.File.getContent(tmxFile);
-        this.xml = Xml.parse(this.tmxFile);
+        this.xml     = Xml.parse(this.tmxFile);
+        this.root    = this.xml.firstElement();
     }
+
+
+    /**
+      Get the root element of the TMX file.
+
+      @return Xml
+     **/
+    public function getRoot() : Xml
+    {
+        return this.root;
+    }
+
 
     /**
       Get all of the data that was parsed from the initial parse
