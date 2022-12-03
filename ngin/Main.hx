cff75566211b62dad9c67b011fc142b09d72fa4d
lukehayes <lukepurplehayes@gmail.com>
2022-12-03 17:39:20 +0000

    Updated main with latest test code.

--- ngin/Main.hx
+++ ngin/Main.hx
@@ -1,21 +1,20 @@
 package ngin;
 
 class Main extends hxd.App {
-
-    var bmp : h2d.Bitmap;
+    var bmp:h2d.Bitmap;
 
     override function init() {
-        var tile = h2d.Tile.fromColor(0x0000FF, 100, 100);
-        bmp = new h2d.Bitmap(tile, s2d);
+        //var tile = h2d.Tile.fromColor(0x0000FF, 100, 100);
+        bmp = new ngin.NGSprite(s2d);
         bmp.x = s2d.width * 0.5;
         bmp.y = s2d.height * 0.5;
     }
+
     override function update(dt:Float) {
         bmp.rotation += 0.1;
     }
-    static function main() {
-        //new Main();
 
-        var tm = new ngin.tilemap.TMXParser("res/Tilemap.tmx");
+    static function main() {
+        new Main();
     }
 }
