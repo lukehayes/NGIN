cc56bf4d0722066cb2f353682fe95020785612f0
lukehayes <lukepurplehayes@gmail.com>
2022-12-03 17:39:08 +0000

    NGEntity.sprite now using instance of NGSprite.

--- ngin/NGEntity.hx
+++ ngin/NGEntity.hx
@@ -1,32 +1,44 @@
 package ngin;
 
+import ngin.NGSprite;
+
 class NGEntity {
 
-	// Graphical object
-	public var sprite : YourSpriteClass;
+    // Graphical object
+    public var sprite : NGSprite;
 
-	// Base coordinates
-	// cx and cy are grid coordinates
-	public var cx : Int;
-	public var cy : Int;
+    // Base coordinates
+    // cx and cy are grid coordinates
+    public var cx : Int;
+    public var cy : Int;
 
-	// xr and yr are ratios (position inside a grid cell)
-	public var xr : Float;
-	public var yr : Float;
+    // xr and yr are ratios (position inside a grid cell)
+    public var xr : Float;
+    public var yr : Float;
 
-	// Resulting coordinates
-	public var xx : Float;
-	public var yy : Float;
+    // Resulting coordinates
+    public var xx : Float;
+    public var yy : Float;
 
-	// Movements
-	public var dx : Float;
-	public var dy : Float;
+    // Movements
+    public var dx : Float;
+    public var dy : Float;
 
-	public function new() {
-		//...
-	}
+    public function new() {
+        this.sprite = new NGSprite();
+    }
 
-	public function update(dt:Float) {
-		//...
-	}
+    public function update(dt:Float) {
+        //...
+    }
+
+    public function setCoordinates(x,y) {
+        xx = x;
+        yy = y;
+        cx = Std.int(xx/16);
+        cy = Std.int(yy/16);
+        xr = (xx-cx*16) / 16;
+        yr = (yy-cy*16) / 16;
+    }
+
 }
