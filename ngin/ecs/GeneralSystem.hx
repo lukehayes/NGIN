d17e13f478eb05cb800f38be6b42785c040b8b4c
Luke Hayes <lukepurplehayes@gmail.com>
2023-03-28 14:41:47 +0100

    Added ecs.GeneralSystem to project

---
+++ ngin/ecs/GeneralSystem.hx
@@ -0,0 +1,15 @@
+package ecs;
+
+import ecs.System;
+
+/**
+  General system for testing all components. Will remove in future.
+  **/
+class GeneralSystem extends System
+{
+    public function new() {}
+
+    public function update(dt:Float)
+    {
+    }
+}
