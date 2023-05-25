d102e08b9b5a6c7fc6cc596d4996ff11214eb56e
Luke Hayes <lukepurplehayes@gmail.com>
2023-05-25 10:27:01 +0100

    Added test code of NGSprite to main

--- ngin/Main.hx
+++ ngin/Main.hx
@@ -2,33 +2,43 @@ package ngin;
 
 import hxd.Window;
 import hxd.Math;
+import h2d.Bitmap;
 
-import ecs.System;
-import ecs.GeneralSystem;
+import ngin.gfx.NGSprite;
 
 class Main extends hxd.App {
 
-    var g : h2d.Graphics;
-    var genSys : GeneralSystem;
+    var bmp : h2d.Bitmap;
+    var spr : NGSprite;
+    var c   : Float;
 
-    override function init() {
-        g = new h2d.Graphics(s2d);
-        genSys = new GeneralSystem();
-        genSys.initEntities(100);
+    function new() 
+    {
+        super();
+        trace('Main Constructed');
     }
 
-    override function update(dt:Float) {
+    override function init() 
+    {
+        this.spr = new NGSprite(0xFF00FF, 100,100,s2d);
+        c = 0.0;
+        spr.x = 300;
+        spr.y = 300;
+    }
 
-        trace(hxd.Timer.fps());
-
-        genSys.moveSystem(dt);
-
-        g.clear();
-            genSys.drawSystem(g);
-        g.endFill();
+    override function update(dt:Float) 
+    {
+        //trace(hxd.Timer.fps());
+        this.c += 0.1;
+        spr.x = spr.x + Math.cos(this.c);
+        spr.y = spr.y + Math.sin(this.c);
     }
 
     static function main() {
+
         new Main();
+
+        // Preload all assets (like images);
+        hxd.Res.initEmbed();
     }
 }
