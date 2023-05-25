7a0eef6d9158879e881c64b234fc885373b05114
Luke Hayes <lukepurplehayes@gmail.com>
2023-05-25 10:26:39 +0100

    Added start of implementation of NGSprite.

--- ngin/gfx/NGSprite.hx
+++ ngin/gfx/NGSprite.hx
@@ -1,25 +1,35 @@
 package ngin.gfx;
 
+import h2d.Tile;
+import h2d.Bitmap;
+import h2d.Scene;
+
 class NGSprite extends h2d.Bitmap
 {
-    /**
-     * Color of the tile **/
-    //public var color : Int;
+    var w : Int;
+    var h : Int;
 
-    /**
-     * Width of the tile **/
-    public var w : Int;
-
-    /**
-     * Height of the tile **/
-    public var h : Int;
-
-    public var instance : h2d.Tile;
-
-
-    public function new(scene: h2d.Scene)
+    public function new(color:Int, w:Int, h:Int, scene:h2d.Scene)
     {
-        super(h2d.Tile.fromColor(0x0000FF,100,100), scene);
-        //this.instance = h2d.Tile.fromColor(0x0000FF, 10,10);
+        super(null, scene);
+        this.w = w;
+        this.h = h;
+    }
+
+    /**
+      * Get the underlying tile instance.
+      * 
+      * @return h2d.Tile
+      */
+    public function get() : h2d.Tile
+    {
+        trace(this.w);
+        trace(this.h);
+
+        return Tile.fromColor(
+                0xFFFF33,
+                this.w,
+                this.h
+        );
     }
 }
