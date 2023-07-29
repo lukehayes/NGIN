0c096a4dbcc56315f680ec6a6ca4a89a2ddb0774
Luke Hayes <lukepurplehayes@gmail.com>
2023-07-29 09:58:33 +0100

    Added ngin.util.NGColor to project.

---
+++ src/ngin/util/NGColor.hx
@@ -0,0 +1,14 @@
+package ngin.util;
+
+/**
+  Static color helpers and functions.
+**/
+class NGColor
+{
+    public static var BLACK : Int = 0x000000;
+    public static var GRAY  : Int = 0x888888;
+    public static var WHITE : Int = 0xFFFFFF;
+    public static var RED   : Int = 0xFF0000;
+    public static var GREEN : Int = 0x00FF00;
+    public static var BLUE  : Int = 0x0000FF;
+}
