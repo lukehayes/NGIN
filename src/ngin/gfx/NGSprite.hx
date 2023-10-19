e773d0b3e900ea31f80109f85498e4b2200e21b1
Luke Hayes <lukepurplehayes@gmail.com>
2023-10-19 10:01:03 +0100

    NGSprite position can now be set inside constructor.

--- src/ngin/gfx/NGSprite.hx
+++ src/ngin/gfx/NGSprite.hx
@@ -12,14 +12,18 @@ class NGSprite extends h2d.Bitmap
      * Constructor.
      *
      * @param scene  The scene to be drawn to.
+     * @param x      The sprites x position.
+     * @param y      The sprites y position.
      * @param color  The color of the sprite.
      * @param width  The width of the sprite.
      * @param height The height of the sprite.
      */
-    public function new(scene: h2d.Scene, color:Int = 0xFF00FF, w:Int = 16, h:Int = 16)
+    public function new(scene: h2d.Scene, x:Float, y:Float, color:Int = 0xFF00FF, w:Int = 16, h:Int = 16)
     {
-        var tile = Tile.fromColor(0xFF00FF, w,h);
+        var tile = Tile.fromColor(color, w,h);
         super(tile, scene);
+        this.x = x;
+        this.y = y;
         this.w = w;
         this.h = h;
     }
@@ -32,7 +36,7 @@ class NGSprite extends h2d.Bitmap
     public function get() : h2d.Tile
     {
         return Tile.fromColor(
-                0xFFFF33,
+                this.color,
                 this.w,
                 this.h
         );
