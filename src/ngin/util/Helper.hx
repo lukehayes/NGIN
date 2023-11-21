81783f45bd2222450f276e2b6602cc27917ed33e
Luke Hayes <lukepurplehayes@gmail.com>
2023-11-21 10:16:04 +0000

    Added ngin.util.Helper.hx

---
+++ src/ngin/util/Helper.hx
@@ -0,0 +1,41 @@
+package ngin.util;
+
+/**
+  Static helper functions are contained in this file.
+**/
+class Helper
+{
+    /**
+      Get input from the W,A,S,D keys and apply this to a
+      targets x and y position. 
+
+      Should be used purely for testing.
+
+      @param target The target to apply movement to.
+      @param dt     Delta time.
+     **/
+    static public function getKeyInput(target:h2d.Object, dt:Float)
+    {
+        final SPEED = 100;
+
+        if(hxd.Key.isDown(hxd.Key.W))
+        {
+            target.y -= SPEED * dt;
+        }
+
+        if(hxd.Key.isDown(hxd.Key.S))
+        {
+            target.y += SPEED * dt;
+        }
+
+        if(hxd.Key.isDown(hxd.Key.A))
+        {
+            target.x -= SPEED * dt;
+        }
+
+        if(hxd.Key.isDown(hxd.Key.D))
+        {
+            target.x += SPEED * dt;
+        }
+    }
+}
