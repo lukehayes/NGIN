69db71db0ecead2e9efd456615fcd046cbeefaa8
Luke Hayes <lukepurplehayes@gmail.com>
2023-11-22 09:59:15 +0000

    Changed constructor param order and color member now interpolates correctly.

--- src/ngin/gfx/NGSprite.hx
+++ src/ngin/gfx/NGSprite.hx
@@ -5,22 +5,27 @@ import h2d.Bitmap;
 
 class NGSprite extends h2d.Bitmap
 {
+    /** Width of the NGSprite. */
     var w : Int;
+
+    /** Height of the NGSprite. */
     var h : Int;
+
+    /** Size of the NGSprite tile. */
     var tileSize: Int = 16;
 
     /**
      * Constructor.
      *
-     * @param scene  The scene to be drawn to.
      * @param x      The sprites x position.
      * @param y      The sprites y position.
-     * @param color  The color of the sprite.
      * @param width  The width of the sprite.
      * @param height The height of the sprite.
+     * @param color  The color of the sprite.
      * @param file   The path of the image to render.
+     * @param scene  The scene to be drawn to.
      */
-    public function new(scene: h2d.Scene,x:Float, y:Float, color:Int = 0xFF00FF, w:Int = 1, h:Int = 1, file : String)
+    public function new(x:Float, y:Float, w:Int = 1, h:Int = 1, color: Int = 0xFF00FF, file : String, scene: h2d.Scene)
     {
         var sprite_file = file + ".png";
         var tile = hxd.Res.loader.load(sprite_file).toTile();
@@ -30,17 +35,21 @@ class NGSprite extends h2d.Bitmap
         this.y = y;
         this.w = w;
         this.h = h;
+
+        // The parent class Drawable holds the property color which is a
+        // h3d.Vector. Here we convert it from an int to a h3d.Vector.
+        this.color = h3d.Vector.fromColor(color);
     }
 
     /**
       * Get the underlying tile instance.
-      * 
-      * @return h2d.Tile
+      *
+      * @return h2d.Tile.
       */
-    public function get() : h2d.Tile
+    public function getTile() : h2d.Tile
     {
         return Tile.fromColor(
-                0xFF0011,
+                this.color.toColor(),
                 this.w,
                 this.h
         );
